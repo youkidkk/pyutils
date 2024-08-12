9335c581a9124717e47bbcbd373dbf371ee70df0
youkidkk <youkidkk80@gmail.com>
2024-08-12 17:06:47 +0900

    パスの標準化を追加

--- pyutils/filesystem.py
+++ pyutils/filesystem.py
@@ -2,7 +2,14 @@ import os
 from typing import Dict, List
 
 
+def _normalize_path(path):
+    sep = os.path.sep
+    return path.replace("/", sep).replace("\\", sep)
+
+
 def walk(parent_dir: str) -> Dict[str, List[str]]:
     return {
-        current_dir: files for current_dir, _, files in os.walk(parent_dir) if files
+        _normalize_path(current_dir): files
+        for current_dir, _, files in os.walk(parent_dir)
+        if files
     }
