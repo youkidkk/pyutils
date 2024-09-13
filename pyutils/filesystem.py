4098252762e53ad14f094ee0874e273751bc9552
youkidkk <youkidkk80@gmail.com>
2024-09-14 08:59:51 +0900

    pathlib.Path で扱うように変更

--- pyutils/filesystem.py
+++ pyutils/filesystem.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Union
 
 
 def _normalize_path(path):
@@ -8,23 +8,29 @@ def _normalize_path(path):
     return path.replace("/", sep).replace("\\", sep)
 
 
-def walk(parent_dir: str) -> Dict[str, List[str]]:
+def walk(parent_dir: Union[Path, str]) -> Dict[Path, List[Path]]:
+    target = Path(parent_dir)
     return {
-        _normalize_path(current_dir): files
-        for current_dir, _, files in os.walk(parent_dir)
+        Path(_normalize_path(current_dir)): [Path(f) for f in files]
+        for current_dir, _, files in os.walk(target)
         if files
     }
 
 
-def walk_files(parent_dir: str, relative=False) -> List[str]:
+def walk_files(parent_dir: Union[Path, str], relative=False) -> List[Path]:
     def rel(path):
         if not relative:
             return path
-        return str(Path(path).relative_to(Path(parent_dir)))
+        return Path(path).relative_to(Path(parent_dir))
 
+    target = Path(parent_dir)
     return [
-        _normalize_path(rel(os.path.join(current_dir, file)))
-        for current_dir, _, files in os.walk(parent_dir)
+        Path(_normalize_path(rel(os.path.join(current_dir, file))))
+        for current_dir, _, files in os.walk(target)
         if files
         for file in files
     ]
+
+
+if __name__ == "__main__":
+    pass
