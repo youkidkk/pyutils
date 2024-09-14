604d4beeb248cda0840c95253e4441d648591ac6
youkidkk <youkidkk80@gmail.com>
2024-09-14 10:12:22 +0900

    コメントを追加

--- pyutils/console.py
+++ pyutils/console.py
@@ -9,6 +9,7 @@ class Console:
         self.last_text_width = 0
 
     def log(self, text: str, newline: bool = False) -> None:
+        """カレント行にログを出力"""
         text_width = texts.width(" " * self.indent_level * Console.indent_width + text)
         if text_width < self.last_text_width:
             text = text + " " * (self.last_text_width - text_width)
@@ -19,13 +20,17 @@ class Console:
         self.last_text_width = text_width
 
     def log_ln(self, text: str = "") -> None:
+        """カレント行にログを出力した後に改行"""
         self.log(text, newline=True)
 
     def indent(self, level: int = 1):
+        """インデントを追加"""
         self.indent_level += level
 
     def unindent(self, level: int = 1):
+        """インデントを削除"""
         self.indent_level -= level
 
     def init_indent(self):
+        """インデントを初期化"""
         self.indent_level = 0
