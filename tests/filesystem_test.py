5f55503b8a0ef54b094c967a915ead3aa8325da6
youkidkk <youkidkk80@gmail.com>
2024-09-21 18:54:50 +0900

    filesystem のテストを追加

---
+++ tests/filesystem_test.py
@@ -0,0 +1,112 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
+
+from pyutils import filesystem as fs
+
+
+def test__normalize_path():
+    func = fs._normalize_path
+
+    patterns = [
+        "test1/test2/test3",
+        "test1\\test2/test3",
+        "test1/test2\\test3",
+    ]
+    correct_result = Path("test1/test2/test3")
+
+    for pattern in patterns:
+        assert func(pattern) == correct_result
+
+
+class TestWalk:
+    @pytest.fixture
+    def init_test_walk(self):
+        with TemporaryDirectory(dir=".") as temp:
+            # temp:
+            self.temp = Path(temp)
+            #   test1
+            #   test2
+            #   test3
+            self.temp.joinpath("test1").touch()
+            self.temp.joinpath("test2").touch()
+            self.temp.joinpath("test3").touch()
+            #   dir1:
+            #     dir1_test1
+            #     dir1_test2
+            (dir1 := self.temp.joinpath("dir1")).mkdir(parents=True)
+            dir1.joinpath("dir1_test1").touch()
+            dir1.joinpath("dir1_test2").touch()
+            #     dir11:
+            #       dir11_test1
+            #       dir11_test2
+            (dir11 := dir1.joinpath("dir11")).mkdir(parents=True)
+            dir11.joinpath("dir11_test1").touch()
+            dir11.joinpath("dir11_test2").touch()
+            #   dir2:
+            #     dir2_test1
+            (dir2 := self.temp.joinpath("dir2")).mkdir(parents=True)
+            dir2.joinpath("dir2_test1").touch()
+            # dir3: empty
+            self.temp.joinpath("dir3").mkdir(parents=True)
+            self.default_result = {
+                self.temp: [
+                    Path("test1"),
+                    Path("test2"),
+                    Path("test3"),
+                ],
+                self.temp.joinpath("dir1"): [
+                    Path("dir1_test1"),
+                    Path("dir1_test2"),
+                ],
+                self.temp.joinpath("dir1", "dir11"): [
+                    Path("dir11_test1"),
+                    Path("dir11_test2"),
+                ],
+                self.temp.joinpath("dir2"): [
+                    Path("dir2_test1"),
+                ],
+            }
+            yield
+        pass
+
+    def test_walk_default_param(self, init_test_walk):
+        # デフォルト
+        # result_type: WalkResultType.FileNameOnly
+        # empty_dir: False
+        assert fs.walk(self.temp) == self.default_result
+
+    def test_walk_absolute(self, init_test_walk):
+        # result_type: WalkResultType.Absolute
+        assert fs.walk(
+            self.temp,
+            fs.WalkResultType.Absolute,
+        ) == {
+            dir.absolute(): [dir.joinpath(file).absolute() for file in files]
+            for dir, files in self.default_result.items()
+        }
+
+    def test_walk_filenameonly(self, init_test_walk):
+        # result_type: WalkResultType.FileNameOnly
+        assert (
+            fs.walk(
+                self.temp,
+                fs.WalkResultType.FileNameOnly,
+            )
+            == self.default_result
+        )
+
+    def test_walk_relative(self, init_test_walk):
+        # result_type: WalkResultType.Relative
+        assert fs.walk(self.temp, fs.WalkResultType.Relative) == {
+            dir: [dir.joinpath(file) for file in files]
+            for dir, files in self.default_result.items()
+        }
+
+    def test_walk_emptydir_true(self, init_test_walk):
+        # empty_dir: False
+        assert fs.walk(self.temp, empty_dir=True) == {
+            **self.default_result,
+            self.temp.joinpath("dir3"): [],
+        }
