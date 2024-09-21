fafde023f620f4ba3e27e1321751bcf95cd7c560
youkidkk <youkidkk80@gmail.com>
2024-09-21 22:07:41 +0900

    型アノテーションを追加

--- pyutils/texts.py
+++ pyutils/texts.py
@@ -3,11 +3,11 @@ import unicodedata
 TRUTHY_VALUES = ["true", "yes", "on", "1"]
 
 
-def width(text):
+def width(text: str) -> int:
     """文字列の幅（半角:1、全角:2）を取得"""
     return sum([2 if unicodedata.east_asian_width(c) in "FWA" else 1 for c in text])
 
 
-def truthy(target: str):
+def truthy(target: str) -> bool:
     target = target.lower()
     return True if target in TRUTHY_VALUES else False
