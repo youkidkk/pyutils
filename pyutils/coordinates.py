a31e126c20f11fd069589f64be012e48883a2a60
youkidkk <youkidkk80@gmail.com>
2024-12-15 15:37:56 +0900

    各クラスに __str__ を追加

--- pyutils/coordinates.py
+++ pyutils/coordinates.py
@@ -6,11 +6,17 @@ class Point:
     x: int
     y: int
 
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
 
 @dataclass(frozen=True)
 class Size:
     width: int
     height: int
+
+    def __str__(self):
+        return f"({self.width} * {self.height})"
 
     def __add__(self, other):
         return Size(
@@ -29,6 +35,9 @@ class Size:
 class Rectangle:
     start_point: Point
     size: Size
+
+    def __str__(self):
+        return f"{self.start_point} - {self.end_point} {self.size}"
 
     @property
     def end_point(self):
--- tests/coordinates_test.py
+++ tests/coordinates_test.py
@@ -7,6 +7,8 @@ def test_point():
     p3 = Point(3, 2)
     p4 = Point(1, 4)
 
+    assert str(p1) == "(1, 2)"
+
     assert p1 == p2
     assert p1 != p3
     assert p1 != p4
@@ -17,6 +19,8 @@ def test_size():
     s2 = Size(1, 2)
     s3 = Size(3, 2)
     s4 = Size(1, 4)
+
+    assert str(s1) == "(1 * 2)"
 
     assert s1 == s2
     assert s1 != s3
@@ -29,6 +33,8 @@ def test_size():
 def test_rectangle():
     r1 = Rectangle(Point(1, 2), Size(3, 4))
     r2 = Rectangle(Point(1, 2), Size(3, 4))
+
+    assert str(r1) == "(1, 2) - (4, 6) (3 * 4)"
 
     assert r1 == r2
     assert r1.end_point == Point(4, 6)
