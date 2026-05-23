a4085d52b8443b5b7128fd72b9451afa1c90270e
youkidkk <youkidkk80@gmail.com>
2026-05-23 11:41:51 +0900

    shoot_datetime のコード改善と撮影日時が取得できない場合に作成日時または更新日時を戻り値とするよう変更

--- pyutils/media/jpeg.py
+++ pyutils/media/jpeg.py
@@ -6,11 +6,18 @@ from typing import Union
 import win32_setctime
 from PIL import ExifTags, Image, UnidentifiedImageError
 
-quality_default = int(os.environ.get("JPG_COMPRESS_QUALITY") or "25")
+from pyutils import filesystem
+
+# 環境変数の読み込み
+quality_default = int(os.environ.get("JPG_COMPRESS_QUALITY", "25"))
 
 
 def _get_exif_tagid(name: str) -> int:
-    return [id for id, val in ExifTags.TAGS.items() if val == name][0]
+    """タグ名から安全にIDを取得する（存在しない場合はValueError）"""
+    tag_id = next((id for id, val in ExifTags.TAGS.items() if val == name), None)
+    if tag_id is None:
+        raise ValueError(f"指定されたEXIFタグ名が見つかりません: {name}")
+    return tag_id
 
 
 tag_id_dtorg = _get_exif_tagid("DateTimeOriginal")
@@ -19,31 +26,41 @@ tag_id_subsec = _get_exif_tagid("SubsecTimeOriginal")
 
 def _shoot_datetime_from_exif(target: Path) -> datetime | None:
     try:
-        img = Image.open(target)
-        exif = img.getexif()
-        if not exif:
-            # EXIFが取得できない場合
-            return
-        exif_dict = exif.get_ifd(ExifTags.IFD.Exif)
-        dtorg = exif_dict.get(tag_id_dtorg)
-        if not dtorg:
-            # 撮影日時が取得できない場合
-            return
-        subsec = exif_dict.get(tag_id_subsec) or "000000"
-        return datetime.strptime(dtorg + subsec, "%Y:%m:%d %H:%M:%S%f")
-    except (UnidentifiedImageError, ValueError):
-        return
+        with Image.open(target) as img:
+            exif = img.getexif()
+            if not exif:
+                # EXIFが取得できない場合
+                return None
+
+            exif_dict = exif.get_ifd(ExifTags.IFD.Exif)
+            dtorg = exif_dict.get(tag_id_dtorg)
+            if not dtorg:
+                # 撮影日時が取得できない場合
+                return None
+
+            # サブ秒（ミリ秒）の取得と成形
+            subsec = exif_dict.get(tag_id_subsec, "000000")
+            # 桁数が可変（2桁や3桁）でも、後ろに0を詰めて6桁（マイクロ秒）に揃える
+            subsec = f"{subsec:<06}"[:6]
+
+            return datetime.strptime(f"{dtorg}.{subsec}", "%Y:%m:%d %H:%M:%S.%f")
+
+    except (UnidentifiedImageError, ValueError, KeyError):
+        return None
 
 
-def shoot_datetime(target_file: Path | str) -> datetime | None:
+def shoot_datetime(target_file: Path | str) -> datetime:
     """撮影日時を取得"""
     target_path = Path(target_file)
-    if not target_path.exists() or not target_path.is_file():
+    if not target_path.is_file():
         raise ValueError(
             f"対象ファイルが存在しないか、ファイルではない: {target_file}",
         )
     if result := _shoot_datetime_from_exif(target_path):
+        # EXIFから撮影日時が取得できた場合 -> その値を返却
         return result
+    # 取得できない場合は作成日時または更新日時を返却
+    return filesystem.get_older_file_timestamp(target_path)
 
 
 def compress(
