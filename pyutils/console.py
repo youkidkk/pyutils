5d135bd7111a806a62ac61fb2a45d986edae8c38
youkidkk <youkidkk80@gmail.com>
2026-05-24 11:33:05 +0900

    コード改善

--- pyutils/console.py
+++ pyutils/console.py
@@ -1,3 +1,5 @@
+import threading
+
 from pyutils import classes, texts
 
 
@@ -10,85 +12,91 @@ class Console:
         init_indent_level: int = 0,
     ):
         self._indent_width: int = indent_width
-        self._top_level = top_level
+        self._top_level: int = top_level
         self._indent_level: int = init_indent_level
-        self._last_text: str = ""
+        self._last_text: str = ""  # 型を str に修正
+
+        self._lock = threading.RLock()
 
     def _indent_space(self) -> str:
-        """インデントレベルに応じたスペースを返却"""
         return " " * (self._top_level + self._indent_level) * self._indent_width
 
-    def _filled_text(self, text) -> str:
-        """インデント、末尾のスペースを付与したテキスト"""
+    def _filled_text(self, text: str) -> str:
         text = f"{self._indent_space()}{text}"
-        if (sp_width := texts.width(self._last_text) - texts.width(text)) > 0:
-            text = text + (" " * sp_width)
+        if (sp_len := len(self._last_text) - len(text)) > 0:
+            text = text + (" " * sp_len * 2)
         return text
 
-    @classes.synchronized
-    def print(self, text: str, end="") -> str:
+    def print(self, text: str, end: str = "") -> str:
         """カレント行にテキストを出力"""
-        filled_text = self._filled_text(texts.remove_ctrl_chars(text))
-        print(
-            "\r" + filled_text,
-            end=end,
-        )
-        self._last_text = filled_text.rstrip()
-        return filled_text
+        with self._lock:
+            filled_text = self._filled_text(texts.remove_ctrl_chars(text))
+            print("\r" + filled_text, end=end, flush=True)
+            self._last_text = filled_text.rstrip()
+            return filled_text
 
     def line_break(self) -> str:
         """改行する"""
-        print()
-        self._last_text = ""
-        return ""
+        with self._lock:
+            print()
+            self._last_text = ""
+            return ""
 
     def print_line(self, text: str = "") -> str:
         """カレント行にテキストを出力後に改行"""
-        self.print(text)
-        return self.line_break()
+        with self._lock:
+            self.print(text)
+            return self.line_break()
 
     def indent(self, level: int = 1) -> int:
         """インデントを追加"""
-        if self._last_text:
-            self.line_break()
-        self._indent_level += level
-        return self._indent_level
+        with self._lock:
+            if self._last_text:
+                self.line_break()
+            self._indent_level += level
+            return self._indent_level
 
     def unindent(self, level: int = 1) -> int:
         """インデントを削除"""
-        if self._last_text:
-            self.line_break()
-        self._indent_level -= level
-        if self._indent_level < 0:
-            self._indent_level = 0
-        return self._indent_level
+        with self._lock:
+            if self._last_text:
+                self.line_break()
+            self._indent_level -= level
+            if self._indent_level < 0:
+                self._indent_level = 0
+            return self._indent_level
 
     def init_indent(self) -> int:
         """インデントを初期化"""
-        if self._last_text:
-            self.line_break()
-        self._indent_level = 0
-        return self._indent_level
+        with self._lock:
+            if self._last_text:
+                self.line_break()
+            self._indent_level = 0
+            return self._indent_level
 
-    def wait_enter(self, text: str = "Press Enter key"):
+    def wait_enter(self, text: str = "Press Enter key") -> None:
         """Enterキーの入力待ち"""
-        self.print(f"{text}: ", end="")
-        input()
-
-    def confirm(self, text: str, default: bool = False):
-        """Y or N の入力"""
-        self.print(f"""{text} ({"Y/n" if default else "y/N"}): """, end="")
-        choice = input()
-        if default:
-            return choice.lower() != "n"
-        else:
-            return choice.lower() == "y"
-
-    def input_int(self, text: str):
-        while True:
+        with self._lock:
             self.print(f"{text}: ", end="")
-            intext = input()
+            input()
+
+    def confirm(self, text: str, default: bool = False) -> bool:
+        """Y or N の入力"""
+        with self._lock:
+            self.print(f"""{text} ({"Y/n" if default else "y/N"}): """, end="")
+            choice = input()
+            if default:
+                return choice.lower() != "n"
+            else:
+                return choice.lower() == "y"
+
+    def input_int(self, text: str) -> int:
+        """整数の入力待ち"""
+        while True:
+            with self._lock:
+                self.print(f"{text}: ", end="")
+                intext = input()
             try:
                 return int(intext)
-            except Exception:
+            except ValueError:
                 pass
--- pyutils/texts.py
+++ pyutils/texts.py
@@ -1,26 +1,25 @@
 import unicodedata
 
+TRUTHY_VALUES = {"true", "yes", "y", "on", "1"}
+FALSY_VALUES = {"false", "no", "n", "off", "0"}
+
 
 def width(text: str) -> int:
     """文字列の幅（半角:1、全角:2）を取得"""
-    return sum([2 if unicodedata.east_asian_width(c) in "FWA" else 1 for c in text])
 
-
-TRUTHY_VALUES = ["true", "yes", "y", "on", "1"]
+    return sum(2 if unicodedata.east_asian_width(c) in "FWA" else 1 for c in text)
 
 
 def truthy(target: str) -> bool:
-    target = target.lower()
-    return True if target in TRUTHY_VALUES else False
-
-
-FALSY_VALUES = ["false", "no", "n", "off", "0"]
+    """文字列が真価を表す値かどうかを判定"""
+    return target.lower() in TRUTHY_VALUES
 
 
 def falsy(target: str) -> bool:
-    target = target.lower()
-    return True if target in FALSY_VALUES else False
+    """文字列が偽価を表す値かどうかを判定"""
+    return target.lower() in FALSY_VALUES
 
 
 def remove_ctrl_chars(target: str) -> str:
+    """制御文字を除去"""
     return "".join(ch for ch in target if unicodedata.category(ch)[0] != "C")
