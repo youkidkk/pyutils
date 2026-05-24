e8b1bcf11a0b8d4e99f8e2a3e919a829f1d8be99
youkidkk <youkidkk80@gmail.com>
2026-05-24 12:12:59 +0900

    テストコードを更新

--- tests/console_test.py
+++ tests/console_test.py
@@ -5,12 +5,12 @@ from pyutils.console import Console
 def test_console():
     console = Console()
     assert console.print("あ\tい\rう\nえお123  ") == "あいうえお123  "
-    assert console.print("あいう12") == "あいう12     "
+    assert console.print("あいう12") == "あいう12      "
     console.indent()
     assert console._last_text == ""
     assert console._indent_level == 1
     assert console.print("あいうえお123") == "  あいうえお123"
-    assert console.print("あいう12") == "  あいう12     "
+    assert console.print("あいう12") == "  あいう12      "
     console.indent()
     assert console._indent_level == 2
     assert console.print("あいうえお123") == "    あいうえお123"
@@ -19,7 +19,7 @@ def test_console():
     console.unindent()
     assert console._indent_level == 1
     assert console.print("あいうえお123") == "  あいうえお123"
-    assert console.print("あいう12") == "  あいう12     "
+    assert console.print("あいう12") == "  あいう12      "
     console.init_indent()
     assert console._indent_level == 0
 
