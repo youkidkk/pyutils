90bd5bdaa128b7e7192b927929e8d35578bd7b3c
youkidkk <youkidkk80@gmail.com>
2026-05-27 21:47:55 +0900

    コメント追加

--- pyutils/config/config_base.py
+++ pyutils/config/config_base.py
@@ -9,20 +9,34 @@ import yaml
 class ConfigBase:
     def __post_init__(self) -> None:
         for field in fields(self.__class__):
+            # フィールドに値を設定
             object.__setattr__(
                 self,
                 field.name,
                 field.type(getattr(self, field.name)),
             )
+            # フィールドのバリデーションを実施
             if hasattr(field.type, "_validate"):
                 field.type._validate(getattr(self, field.name))
 
     @classmethod
     def load(cls, config_path: Path = Path("./config.yaml")) -> Self:
+        """設定ファイルを読み込み、インスタンスを返却する。
+
+        Args:
+            raw_dict (dict, optional): 設定ファイルのパス。 デフォルト ./config.yaml
+
+        Returns:
+            Self: 設定クラスのインスタンス。
+        """
         with open(config_path, encoding="utf-8") as config_file:
             config_dict = yaml.safe_load(config_file)
             return cls(**cls._convert(config_dict))
 
     @classmethod
     def _convert(cls, raw_dict: dict) -> dict:
+        """
+        設定ファイルからインスタンス変数に設定する型に変換する。
+        何らかの変換が必要な場合、派生クラス側で処理を追加すること。
+        """
         return raw_dict
--- pyutils/config/types.py
+++ pyutils/config/types.py
@@ -2,52 +2,70 @@ from pathlib import Path
 
 
 class Directory(Path):
+    """ディレクトリ型"""
+
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls, *args, **kwargs)
 
     @classmethod
     def _validate(cls, value: Path) -> None:
         if value.is_file():
+            # ファイルである場合はエラー
             raise ValueError(f"{value}: 対象パスがファイルとして存在します。")
 
 
 class ExistingDirectory(Directory):
+    """存在するディレクトリを示す型"""
+
     @classmethod
     def _validate(cls, value: Path) -> None:
         super()._validate(value)
         if not value.is_dir():
+            # ディレクトリとして存在しない場合はエラー
             raise ValueError(f"{value}: 対象ディレクトリが存在しません。")
 
 
 class EmptyDirectory(ExistingDirectory):
+    """空のディレクトリを示す型"""
+
     @classmethod
     def _validate(cls, value: Path) -> None:
         super()._validate(value)
         if any(value.iterdir()):
+            # 空のディレクトリではない場合はエラー
             raise ValueError(f"{value}: 対象ディレクトリが空ではありません。")
 
 
 class EmptyOrNonExistingDirectory(Directory):
+    """空または存在しないディレクトリを示す型"""
+
     @classmethod
     def _validate(cls, value: Path) -> None:
         super()._validate(value)
         if value.is_dir() and any(value.iterdir()):
+            # ディレクトリとして存在し、空ではない場合はエラー
             raise ValueError(f"{value}: 対象ディレクトリが空ではありません。")
 
 
 class File(Path):
+    """ファイル型"""
+
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls, *args, **kwargs)
 
     @classmethod
     def _validate(cls, value: Path) -> None:
         if value.is_dir():
+            # ディレクトリである場合はエラー
             raise ValueError(f"{value}: 対象パスがディレクトリとして存在します。")
 
 
 class ExistingFile(File):
+    """存在するファイルを示す型"""
+
     @classmethod
     def _validate(cls, value: Path) -> None:
         super()._validate(value)
         if not value.is_file():
+            # ファイルとして存在しない場合はエラー
             raise ValueError(f"{value}: 対象ファイルが存在しません。")
