fbd915f1ca6f19123fd994a1302ec2a7ef0666df
youkidkk <youkidkk80@gmail.com>
2026-06-01 08:36:16 +0900

    PathBase のコンストラクタで Path オブジェクトを返す方式に変更

--- pyutils/config/types.py
+++ pyutils/config/types.py
@@ -1,89 +1,106 @@
-from abc import ABC, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from pathlib import Path
-from typing import Self
+from typing import Any
 
 
 class FieldTypeBase(ABC):
-    def __init__(self: Self, value: any) -> None:
-        self._value = value
-
+    @classmethod
     @abstractmethod
-    def _validate(self) -> None:
+    def _validate(cls, value: Any) -> Any:
         pass
 
-    def __call__(self: Self) -> any:
-        return self._value
+
+class _PathBaseValidationMeta(ABCMeta):
+    """isinstance(obj, Class) の挙動をカスタマイズするメタクラス"""
+
+    def __instancecheck__(cls, instance: Any) -> bool:
+        if not isinstance(instance, Path):
+            return False
+        try:
+            cls._validate(instance)
+            return True
+        except ValueError:
+            return False
 
 
-class PathBase(FieldTypeBase):
+class PathBase(FieldTypeBase, metaclass=_PathBaseValidationMeta):
     """パスを示す型"""
 
-    def __init__(self: Self, value: str) -> None:
-        self._value = Path(value)
+    def __new__(cls, org_value: Any) -> Path:
+        return cls._validate(Path(org_value))
 
-    def _validate(self) -> None:
-        super()._validate()
-
-    def __getattr__(self, name):
-        """self._value(Pathオブジェクト)への移譲"""
-        return getattr(self._value, name)
+    @classmethod
+    def _validate(cls, value: Path) -> Path:
+        return value
 
 
 class Directory(PathBase):
     """ディレクトリ型"""
 
-    def _validate(self) -> None:
-        super()._validate()
-        if self().is_file():
+    @classmethod
+    def _validate(cls, value: Path) -> Path:
+        value = super()._validate(value)
+        if value.is_file():
             # ファイルである場合はエラー
-            raise ValueError(f"対象パスがファイルとして存在します {self()}")
+            raise ValueError(f"対象パスがファイルとして存在します {value}")
+        return value
 
 
 class ExistingDirectory(Directory):
     """存在するディレクトリを示す型"""
 
-    def _validate(self) -> None:
-        super()._validate()
-        if not self().is_dir():
+    @classmethod
+    def _validate(cls, value: Path) -> Path:
+        value = super()._validate(value)
+        if not value.is_dir():
             # ディレクトリとして存在しない場合はエラー
-            raise ValueError(f"対象ディレクトリが存在しません {self()}")
+            raise ValueError(f"対象ディレクトリが存在しません {value}")
+        return value
 
 
 class EmptyDirectory(ExistingDirectory):
     """空のディレクトリを示す型"""
 
-    def _validate(self) -> None:
-        super()._validate()
-        if any(self().iterdir()):
+    @classmethod
+    def _validate(cls, value: Path) -> Path:
+        value = super()._validate(value)
+        if any(value.iterdir()):
             # 空のディレクトリではない場合はエラー
-            raise ValueError(f"対象ディレクトリが空ではありません {self()}")
+            raise ValueError(f"対象ディレクトリが空ではありません {value}")
+        return value
 
 
 class EmptyOrNonExistingDirectory(Directory):
     """空または存在しないディレクトリを示す型"""
 
-    def _validate(self) -> None:
-        super()._validate()
-        if self().is_dir() and any(self().iterdir()):
+    @classmethod
+    def _validate(cls, value: Path) -> Path:
+        value = super()._validate(value)
+        if value.is_dir() and any(value.iterdir()):
             # ディレクトリとして存在し、空ではない場合はエラー
-            raise ValueError(f"対象ディレクトリが空ではありません {self()}")
+            raise ValueError(f"対象ディレクトリが空ではありません {value}")
+        return value
 
 
 class File(PathBase):
     """ファイルを示す型"""
 
-    def _validate(self) -> None:
-        super()._validate()
-        if self().is_dir():
+    @classmethod
+    def _validate(cls, value: Path) -> Path:
+        value = super()._validate(value)
+        if value.is_dir():
             # ディレクトリである場合はエラー
-            raise ValueError(f"対象パスがディレクトリとして存在します {self()}")
+            raise ValueError(f"対象パスがディレクトリとして存在します {value}")
+        return value
 
 
 class ExistingFile(File):
     """存在するファイルを示す型"""
 
-    def _validate(self) -> None:
-        super()._validate()
-        if not self().is_file():
+    @classmethod
+    def _validate(cls, value: Path) -> Path:
+        value = super()._validate(value)
+        if not value.is_file():
             # ファイルとして存在しない場合はエラー
-            raise ValueError(f"対象ファイルが存在しません {self()}")
+            raise ValueError(f"対象ファイルが存在しません {value}")
+        return value
