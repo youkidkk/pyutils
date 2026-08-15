dd9ffa42c103454fa4786b2b4d40ec50c1783ec3
youkidkk <youkidkk80@gmail.com>
2026-08-15 12:49:03 +0900

    validation を追加

---
+++ pyutils/validation.py
@@ -0,0 +1,63 @@
+from collections.abc import Callable
+from functools import wraps
+from typing import Any
+
+
+class InterruptValidation(Exception):
+    """バリデーション処理を途中で中断するための例外"""
+
+
+def rule(err_msg_default: str, interrupt_default: bool = False):
+    """バリデーション関数をルール化するデコレータ"""
+
+    def decorator(func: Callable[..., bool]):
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Callable[[Any], str | None]:
+            err_msg_tmpl = kwargs.pop("err_msg", err_msg_default)
+            interrupt = kwargs.pop("interrupt", interrupt_default)
+
+            def validator(value: Any) -> str | None:
+                if func(value, *args, **kwargs):
+                    return None
+
+                # エラーメッセージの生成
+                try:
+                    err = err_msg_tmpl.format(*args, value=value, **kwargs)
+                except (IndexError, KeyError):
+                    err = err_msg_tmpl
+
+                if interrupt:
+                    raise InterruptValidation(err)
+                return err
+
+            return validator
+
+        return wrapper
+
+    return decorator
+
+
+@rule("{0}桁で入力してください")
+def length(value: str, len_: int) -> bool:
+    return len(value) == len_
+
+
+@rule("{0}桁以上で入力してください")
+def min_length(value: str, len_: int) -> bool:
+    return len(value) >= len_
+
+
+@rule("{0}桁以下で入力してください")
+def max_length(value: str, len_: int) -> bool:
+    return len(value) <= len_
+
+
+def validate(value: Any, *rules: Callable[[Any], str | None]) -> list[str]:
+    errors: list[str] = []
+    try:
+        for r in rules:
+            if err := r(value):
+                errors.append(err)
+    except InterruptValidation as e:
+        errors.append(str(e))
+    return errors
